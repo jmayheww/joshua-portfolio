f3e46d2a30ae7e3a3ff57207a2435c729fe1bc83
Joshua <joshsmayhew@gmail.com>
2023-08-27 14:20:33 +0700

    install daisyui and specify plugin for use in tailwind config file, create theme for styling across components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f25ecf",
+          secondary: "#74e088",
+          accent: "#d8ef8b",
+          neutral: "#242433",
+          "base-100": "#27414f",
+          info: "#2650d9",
+          success: "#5becbb",
+          warning: "#f4b767",
+          error: "#fa6257",
+        },
+      },
+    ],
+  },
+};

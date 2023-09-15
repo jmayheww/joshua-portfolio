6257c2a277b0971c69b442d6538efb0772bbaf2e
Joshua <joshsmayhew@gmail.com>
2023-09-15 18:13:58 +0700

    match about margins and responsive display to mirror hero section dimensions

--- app/sections/About.js
+++ app/sections/About.js
@@ -5,12 +5,12 @@ export const About = () => {
   const paragraphClasses = "text-sm sm:text-base font-jetbrains-mono";
 
   return (
-    <section className="flex flex-col items-center py-16 px-4 sm:px-6 lg:px-8 space-y-4 sm:space-y-6 md:space-y-12 lg:space-y-16 max-w-4xl mx-auto">
+    <section className="flex flex-col items-center py-16 px-4 sm:px-6 md:px-8 lg:px-8 space-y-4 sm:space-y-6 md:space-y-12 lg:space-y-16 max-w-4xl mx-auto">
       <SectionHeader title="About Me" />
 
-      <div className="flex flex-col md:flex-row w-full items-center md:space-x-10">
+      <div className="flex flex-col md:flex-row w-full items-start md:space-x-10">
         {/* Portrait */}
-        <div className="relative w-3/4 md:w-1/3 mx-auto md:mx-0 mb-4 md:mb-0">
+        <div className="relative w-2/3 sm:w-1/2 md:w-1/3 mx-auto md:mx-0 mb-8 md:mb-0">
           <div className="absolute -bottom-3 -right-4 w-full h-full border-4 border-success rounded-sm z-0"></div>
           <div className="relative w-full z-10 overflow-hidden rounded-sm">
             <Image
@@ -24,19 +24,21 @@ export const About = () => {
             <div className="absolute top-0 left-0 w-full h-full bg-success mix-blend-multiply hover:opacity-0 transition-opacity duration-300"></div>
           </div>
         </div>
+
         {/* Tagline */}
-        <div className="flex flex-col space-y-4 w-full md:w-2/3 text-center md:text-left">
+        <div className="flex flex-col space-y-4 w-full md:w-2/3 text-left md:pl-10">
           {/* Mobile Tagline */}
           <p
-            className={`text-xl sm:text-2xl md:text-3xl text-white ${paragraphClasses} block md:hidden`}
+            className={`text-xl sm:text-2xl md:text-3xl text-white ${paragraphClasses} block md:hidden max-w-xs`}
           >
             Code that's clean, designs that resonate, experience that lasts.{" "}
             <span className="text-primary">It's what I do.</span>
           </p>
+
           {/* Desktop Tagline */}
           <div className="hidden md:block">
             <p
-              className={`text-xl sm:text-2xl md:text-3xl text-white ${paragraphClasses}`}
+              className={`text-xl sm:text-2xl md:text-3xl text-white ${paragraphClasses} block md:hidden max-w-md mx-auto text-center`}
             >
               Code that's clean.
             </p>
@@ -60,34 +62,32 @@ export const About = () => {
       </div>
 
       {/* More About Text */}
-      <div className="max-w-xl md:max-w-2xl space-y-6">
-        <div>
-          <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-inter tracking-tight text-white mb-4">
-            Paralegal to Programmer
-          </h3>
-          <p
-            className={`${paragraphClasses} md:text-lg leading-relaxed text-gray-300`}
-          >
-            Navigating the world of legal documentation refined my analytical
-            edge. Today, I harness that rigor and precision in software
-            development, ensuring every line of code is not just functional, but
-            purposeful.
-          </p>
-        </div>
+      <div>
+        <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-inter tracking-tight text-white mb-4">
+          Paralegal to Programmer
+        </h3>
+        <p
+          className={`${paragraphClasses} md:text-lg leading-relaxed text-gray-300`}
+        >
+          Navigating the world of legal documentation refined my analytical
+          edge. Today, I harness that rigor and precision in software
+          development, ensuring every line of code is not just functional, but
+          purposeful.
+        </p>
+      </div>
 
-        <div>
-          <h3 className="text-lg sm:text-xl md:text-2xl font-inter tracking-tight text-primary mb-4">
-            Brewing Code with Purpose.
-          </h3>
-          <p
-            className={`${paragraphClasses} md:text-lg leading-relaxed text-gray-400`}
-          >
-            I channel a universal ethos - a synergized brew of innovation,
-            drive, and caffeinated creativity - in every project I undertake. My
-            aim? To craft digital solutions that genuinely resonate with users
-            while strategically elevating businesses.
-          </p>
-        </div>
+      <div>
+        <h3 className="text-lg sm:text-xl md:text-2xl font-inter tracking-tight text-primary mb-4">
+          Brewing Code with Purpose.
+        </h3>
+        <p
+          className={`${paragraphClasses} md:text-lg leading-relaxed text-gray-400`}
+        >
+          I channel a universal ethos - a synergized brew of innovation, drive,
+          and caffeinated creativity - in every project I undertake. My aim? To
+          craft digital solutions that genuinely resonate with users while
+          strategically elevating businesses.
+        </p>
       </div>
     </section>
   );

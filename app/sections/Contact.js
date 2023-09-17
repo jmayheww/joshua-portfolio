9340f482b48d1a58bdb8438693fcdf5f6ea60770
Joshua <joshsmayhew@gmail.com>
2023-09-17 21:35:50 +0700

    edit contact section text and style button

--- app/sections/Contact.js
+++ app/sections/Contact.js
@@ -1,27 +1,37 @@
 export const Contact = () => {
+  const buttonClasses = `
+    inline-flex items-center justify-center
+    py-3 md:py-4 px-5 md:px-7
+    text-sm md:text-lg
+    bg-neutral text-white
+    border border-primary-500
+    rounded-sm shadow-md
+    transition-all transform
+    hover:bg-primary hover:shadow-xl hover:-translate-y-1
+    active:scale-95 focus:outline-none focus:ring focus:ring-primary-200
+    font-inter font-bold
+  `;
+
   return (
-    <div className="flex flex-col items-center justify-center h-full py-12">
-      <div className="text-center max-w-lg">
-        <h2 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-6">
-          Get In Touch
+    <div className="flex flex-col items-center justify-center mt-12 mb-12 md:mt-16 md:mb-16">
+      <div className="text-center w-full max-w-lg px-4 md:px-6">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-6 tracking-tight">
+          What's Next?
         </h2>
-        <p className="text-lg lg:text-xl mb-8">
-          I&rsquo;m always open to new opportunities and collaborations. Feel
-          free to reach out to me.
+        <p className="text-base md:text-lg text-gray-300 mb-6 leading-relaxed">
+          Open to{" "}
+          <span className="text-primary">projects and opportunities</span>. Have
+          an idea brewing or a problem that needs solving? Just looking to chat?{" "}
+          <span className="text-success">I'd love to hear from you!</span> My
+          inbox is always open, and my coffee is always fresh.
         </p>
-        {/* <p className="mt-4 text-sm sm:text-base md:text-lg leading-relaxed font-jetbrains-mono text-gray-400">
-          If you're seeking a developer who seamlessly merges tech expertise
-          with a keen understanding of end-user needs and business objectives,{" "}
-          <a href="/contact" className="underline text-primary">
-            let's connect
-          </a>
-        </p> */}
         <a
           href="mailto:joshsmayhew@gmail.com"
-          className="inline-block bg-primary text-white font-medium text-lg py-3 px-8 rounded-sm transition duration-300 hover:bg-opacity-80 mb-8 shadow-md"
+          className={buttonClasses}
           aria-label="Email Me"
         >
-          Email Me
+          Say Hello
+          {/* Optional: Add an SVG icon here if you'd like */}
         </a>
       </div>
     </div>

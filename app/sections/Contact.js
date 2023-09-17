47633fd8a51dc6055043b2926852eebc86125702
Joshua <joshsmayhew@gmail.com>
2023-09-17 21:39:22 +0700

    fixed styling error with apostrophes in contact

--- app/sections/Contact.js
+++ app/sections/Contact.js
@@ -16,14 +16,14 @@ export const Contact = () => {
     <div className="flex flex-col items-center justify-center mt-12 mb-12 md:mt-16 md:mb-16">
       <div className="text-center w-full max-w-lg px-4 md:px-6">
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-6 tracking-tight">
-          What's Next?
+          What&rsquo;s Next?
         </h2>
         <p className="text-base md:text-lg text-gray-300 mb-6 leading-relaxed">
           Open to{" "}
           <span className="text-primary">projects and opportunities</span>. Have
           an idea brewing or a problem that needs solving? Just looking to chat?{" "}
-          <span className="text-success">I'd love to hear from you!</span> My
-          inbox is always open, and my coffee is always fresh.
+          <span className="text-success">I&rsquo;d love to hear from you!</span>{" "}
+          My inbox is always open, and my coffee is always fresh.
         </p>
         <a
           href="mailto:joshsmayhew@gmail.com"

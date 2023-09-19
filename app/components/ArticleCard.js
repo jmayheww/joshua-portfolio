52176081a895297c29731ad4183855a29c863a6b
Joshua <joshsmayhew@gmail.com>
2023-09-19 20:54:05 +0700

    style article and project cards and sections

--- app/components/ArticleCard.js
+++ app/components/ArticleCard.js
@@ -1,37 +1,39 @@
 export const ArticleCard = ({ article }) => {
   const cardClasses = `
-      border border-gray-300
-      rounded-sm
-      shadow-lg
-      overflow-hidden
-      hover:shadow-xl
-      transition-shadow
-      duration-300
-      bg-neutral
-      text-white
-    `;
+        m-4
+        max-w-3xl
+        mx-auto
+        rounded-sm
+        shadow-lg
+        overflow-hidden
+        hover:shadow-xl hover:scale-105
+        transition-shadow duration-300
+        bg-neutral
+        text-white
+        transform transition-transform duration-200  // Add transition for the transform property
+      `;
 
   const headerClasses = `
-      p-4
-      bg-gradient-to-r
-      from-primary
-      to-neutral
-      font-jetbrains-mono
-      font-bold
-    `;
+        p-4
+        bg-gradient-to-r
+        from-primary
+        to-neutral
+        font-jetbrains-mono
+        font-bold
+      `;
 
   const contentClasses = `
-      p-4
-      font-jetbrains-mono
-      text-gray-300
-    `;
+        p-4
+        font-jetbrains-mono
+        text-gray-300
+      `;
 
   const linkClasses = `
-      text-success
-      hover:underline
-      transition-colors
-      duration-300
-    `;
+        text-success
+        hover:underline
+        transition-colors
+        duration-300
+      `;
 
   return (
     <div className={cardClasses}>
--- app/sections/Writing.js
+++ app/sections/Writing.js
@@ -13,7 +13,7 @@ export const Writing = () => {
         view the archive
       </a>
 
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="w-full grid grid-cols-1 gap-8">
         {articles.map((article) => (
           <ArticleCard key={article.title} article={article} />
         ))}

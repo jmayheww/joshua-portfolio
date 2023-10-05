981cc6b872e9a2f82ba4e628c8d082f21071f3ce
Joshua <joshsmayhew@gmail.com>
2023-10-05 16:28:37 +0700

    add copyright info to footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,11 +2,14 @@ import { SocialIcons } from "./SocialIcons";
 
 export const Footer = () => {
   return (
-    <footer className="w-full py-8 bg-base-100">
-      {" "}
-      {/* Ensure footer is full width */}
-      <div className="container mx-auto max-w-screen-md flex justify-center space-x-6">
-        <SocialIcons />
+    <footer className="w-full py-4 bg-base-100">
+      <div className="container mx-auto max-w-screen-md px-4">
+        <div className="flex flex-col items-center space-y-2 md:space-y-4">
+          <SocialIcons />
+          <p className="text-gray-500 text-center font-mono">
+            ©{new Date().getFullYear()} Joshua Mayhew.
+          </p>
+        </div>
       </div>
     </footer>
   );

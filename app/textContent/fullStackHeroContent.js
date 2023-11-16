619188b8ca525babb30eebf9dec0eb78dbe75191
Joshua <joshsmayhew@gmail.com>
2023-11-16 14:36:18 +0700

    create fullStackHeroContent to extract text content and allow for easier and dynamic maintenance of hero section

---
+++ app/textContent/fullStackHeroContent.js
@@ -0,0 +1,24 @@
+// heroContent.js - Contains content for the Hero section.
+
+export const fullStackHeroContent = {
+  greeting: "Hey there, I’m",
+  name: "Joshua Mayhew.",
+  nameHighlightClass: "text-success",
+  craftingText: "I craft ",
+  typingAttributes: [
+    "clean",
+    "robust",
+    "scalable",
+    "responsive",
+    "efficient",
+    "secure",
+    "dynamic",
+  ],
+  craftingHighlight: "code that works.",
+  description:
+    "I’m a full-stack developer blending code and coffee in the heart of Los Angeles. I craft digital products that uplift users and bolster businesses. Let’s connect where caffeine meets creativity.",
+  callToAction: "See my work",
+  callToActionLink: "#work",
+};
+
+// Export other content if needed
---
+++ app/textContent/sfmcHeroContent.js
@@ -0,0 +1,27 @@
+// salesForceHeroContent.js - Contains content for the Salesforce Marketing Cloud Hero section.
+
+export const salesForceHeroContent = {
+  greeting: "Hey there, I’m",
+  name: "Joshua Mayhew.",
+  nameHighlightClass: "text-success",
+  craftingText: "Specializing in ",
+  typingAttributes: [
+    "engaging",
+    "personalized",
+    "targeted",
+    "responsive",
+    "data-driven",
+    "automated",
+    "optimized",
+  ],
+  craftingHighlight: "SFMC Email Campaigns.",
+  description:
+    "Blending marketing insight with technical expertise, I create Salesforce Marketing Cloud email campaigns that resonate with audiences and drive results. Ready to elevate your email marketing? Let’s ",
+  emailLinkText: "connect",
+  emailLink: "mailto:joshsmayhew@gmail.com",
+  descriptionEnd: "and make every message count.",
+  callToAction: "Explore my work",
+  callToActionLink: "#work",
+};
+
+// Export other content if needed

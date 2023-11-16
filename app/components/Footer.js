d8c973fe94c15c93216656b73a3ff4400da57676
Joshua <joshsmayhew@gmail.com>
2023-11-16 18:04:00 +0700

    changed color for more visual contrast in footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div className="container mx-auto max-w-screen-md px-4">
         <div className="flex flex-col items-center space-y-2 md:space-y-4">
           <SocialIcons />
-          <p className="text-gray-500 text-center font-mono">
+          <p className="text-gray-300 text-center font-mono">
             ©{new Date().getFullYear()} Joshua Mayhew.
           </p>
         </div>

fbf0c69eb297202b2d9d0dfdef3404fc193d549a
Joshua <joshsmayhew@gmail.com>
2023-11-16 17:48:14 +0700

    create new TabButtons component to handle switching between projects

---
+++ app/components/TabButtons.js
@@ -0,0 +1,22 @@
+export const TabButtons = ({ activeTab, setActiveTab }) => {
+  return (
+    <div className="tabs tabs-boxed mb-8 flex justify-center">
+      {["email", "fullstack"].map((tab) => {
+        const isActive = tab === activeTab;
+        const tabName =
+          tab === "email" ? "Email Development" : "Full Stack Development";
+
+        return (
+          <a
+            key={tab}
+            onClick={() => setActiveTab(tab)}
+            className={`tab tab-lg m-1 ${isActive ? "tab-active" : ""}
+                          text-sm sm:text-base flex-1 text-center`}
+          >
+            {tabName}
+          </a>
+        );
+      })}
+    </div>
+  );
+};

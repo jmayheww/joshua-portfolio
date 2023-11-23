8cccfb3c3e90f8aeddd3751e52cdafa4ce8e78e3
Joshua <joshsmayhew@gmail.com>
2023-11-23 14:21:02 +0700

    created useIntersectionObserver hook to more easily import dynamic scrolling and loading functionality

---
+++ app/hooks/useIntersectionObserver.js
@@ -0,0 +1,25 @@
+import { useEffect, useState, useRef } from "react";
+
+export const useIntersectionObserver = (options) => {
+  const [isIntersecting, setIntersecting] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        setIntersecting(true);
+        observer.disconnect();
+      }
+    }, options);
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, options]);
+
+  return [ref, isIntersecting];
+};

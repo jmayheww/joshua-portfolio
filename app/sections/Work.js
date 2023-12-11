d1ed34f113c0b697f400656fa17087ac665680dd
Joshua <joshsmayhew@gmail.com>
2023-12-11 17:18:06 +0700

    refactor work section for readability and modularity

--- app/sections/Work.js
+++ app/sections/Work.js
@@ -6,10 +6,50 @@ import { projects } from "../data";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
-const fadeIn = {
-  hidden: { opacity: 0, y: 20 }, // Adjusted y value for a subtle animation
-  visible: { opacity: 1, y: 0 },
+const fadeInFromRight = {
+  hidden: { opacity: 0, x: 20 },
+  visible: { opacity: 1, x: 0 },
 };
+
+const ProjectList = ({ projectType, projects }) => {
+  const filteredProjects =
+    projectType === "email"
+      ? projects.emailProjects
+      : projects.fullStackProjects;
+
+  return (
+    <>
+      {filteredProjects.map((project, index) => (
+        <InViewProjectCard
+          key={project.title}
+          project={project}
+          delay={index * 0.1}
+        />
+      ))}
+    </>
+  );
+};
+
+const InViewProjectCard = React.memo(({ project, delay }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={fadeInFromRight}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      transition={{ duration: 0.7, delay: delay }}
+    >
+      <ProjectCard project={project} />
+    </motion.div>
+  );
+});
+
+InViewProjectCard.displayName = "InViewProjectCard";
 
 export const Work = () => {
   const [activeTab, setActiveTab] = useState("email");
@@ -17,46 +57,10 @@ export const Work = () => {
   return (
     <section className="flex flex-col items-center ...">
       <SectionHeader title="Work" className="mb-2" />
-
       <TabButtons activeTab={activeTab} setActiveTab={setActiveTab} />
-
-      <div className="grid grid-cols-1 gap-8">
-        {activeTab === "email" &&
-          projects.emailProjects.map((project, index) => (
-            <InViewProjectCard
-              key={project.title}
-              project={project}
-              delay={index * 0.1}
-            />
-          ))}
-        {activeTab === "fullstack" &&
-          projects.fullStackProjects.map((project, index) => (
-            <InViewProjectCard
-              key={project.title}
-              project={project}
-              delay={index * 0.1}
-            />
-          ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <ProjectList projectType={activeTab} projects={projects} />
       </div>
     </section>
-  );
-};
-
-const InViewProjectCard = ({ project, delay }) => {
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
-
-  return (
-    <motion.div
-      ref={ref}
-      variants={fadeIn}
-      initial="hidden"
-      animate={inView ? "visible" : "hidden"}
-      transition={{ duration: 0.5, delay: delay }}
-    >
-      <ProjectCard project={project} />
-    </motion.div>
   );
 };

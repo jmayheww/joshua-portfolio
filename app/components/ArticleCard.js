4809a6f65507a6d1dfa48d248714827622a33873
Joshua <joshsmayhew@gmail.com>
2023-12-11 23:25:49 +0700

    adjust fade in effects for articles and display them side by side on larger devices

--- app/components/ArticleCard.js
+++ app/components/ArticleCard.js
@@ -1,8 +1,10 @@
-import { FaArrowRight } from "react-icons/fa";
 import { useState, useEffect } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 export const ArticleCard = ({ article }) => {
   const [imageLoaded, setImageLoaded] = useState(false);
+  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
+
   const onImageLoad = () => {
     setImageLoaded(true);
   };
@@ -19,15 +21,18 @@ export const ArticleCard = ({ article }) => {
     return baseURL + transformations + imagePath;
   };
 
+  // Function to check if the screen is large (you might want to use a more React-friendly approach)
+  const isLargeScreen = () => window.innerWidth >= 1024;
+
   return (
-    <div className="card bg-neutral rounded-sm shadow-md  p-4 md:p-6 mx-auto lg:max-w-3xl">
+    <div className="card bg-neutral rounded-md shadow-md p-4 md:p-6 mx-auto">
       {/* Image */}
       {article.image && (
-        <div className="relative w-full aspect-w-16 aspect-h-9 md:aspect-w-4 md:aspect-h-3 lg:aspect-w-16 lg:aspect-h-9 xl:aspect-w-4 xl:aspect-h-3 rounded-t-sm overflow-hidden mb-4">
+        <div className="relative w-full aspect-w-16 aspect-h-9 md:aspect-w-16 md:aspect-h-9 lg:aspect-w-16 lg:aspect-h-9 xl:aspect-w-16 xl:aspect-h-9 rounded-t-md overflow-hidden mb-4">
           {!imageLoaded && (
             <div
               className="animate-pulse bg-gray-200"
-              style={{ height: "400px" }}
+              style={{ paddingTop: "56.25%" }} // 16:9 aspect ratio
             ></div>
           )}
           <img
@@ -45,14 +50,26 @@ export const ArticleCard = ({ article }) => {
       {/* Content */}
       <div className="space-y-4">
         {/* Title with Gradient */}
-        <h3 className="text-xl font-semibold mb-2 bg-gradient-to-r from-primary to-neutral font-mono font-bold p-2 rounded-sm">
+        <h3 className="text-xl font-semibold mb-2 bg-gradient-to-r from-primary to-neutral font-mono font-bold p-2 rounded-md">
           {article.title}
         </h3>
 
         {/* Description */}
-        <p className="text-gray-300 mb-4 font-inter text-gray-300 leading-relaxed">
+        <p
+          className={`text-gray-300 mb-4 font-inter text-gray-300 ${
+            isLargeScreen() && !isDescriptionExpanded ? "line-clamp-3" : ""
+          }`}
+        >
           {article.description}
         </p>
+        {isLargeScreen() && article.description.length > 100 && (
+          <button
+            onClick={() => setIsDescriptionExpanded(!isDescriptionExpanded)}
+            className="text-primary hover:text-primary-dark text-sm"
+          >
+            {isDescriptionExpanded ? "See Less" : "See More"}
+          </button>
+        )}
 
         {/* Read More Link */}
         <a
--- app/sections/Writing.js
+++ app/sections/Writing.js
@@ -10,12 +10,17 @@ const fadeIn = {
   visible: { opacity: 1, y: 0 },
 };
 
+const fadeInFromRight = {
+  hidden: { opacity: 0, x: 80 }, // Adjusted x value for a subtle animation
+  visible: { opacity: 1, x: 0 },
+};
+
 export const Writing = () => {
   return (
-    <section className="flex flex-col items-center py-16 px-4 sm:px-6 md:px-8 lg:px-8 space-y-4 sm:space-y-6 md:space-y-12 lg:space-y-16 max-w-4xl mx-auto">
+    <section className="flex flex-col items-center py-16 px-4 sm:px-6 md:px-8 lg:px-8 space-y-4 sm:space-y-6 md:space-y-12 lg:space-y-16 ">
       <SectionHeader title="Writing" className="mb-2" />
 
-      <div className="w-full grid grid-cols-1 gap-8 mb-8 sm:mb-10 md:mb-12 ">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8 sm:mb-10 md:mb-12 ">
         {articles.map((article, index) => (
           <InViewArticleCard
             key={article.title}
@@ -42,16 +47,16 @@ export const Writing = () => {
 const InViewArticleCard = ({ article, delay }) => {
   const [ref, inView] = useInView({
     triggerOnce: true,
-    threshold: 0.1,
+    threshold: 0.3,
   });
 
   return (
     <motion.div
       ref={ref}
-      variants={fadeIn}
+      variants={fadeInFromRight}
       initial="hidden"
       animate={inView ? "visible" : "hidden"}
-      transition={{ duration: 0.5, delay: delay }}
+      transition={{ duration: 0.7, delay: delay }}
     >
       <ArticleCard article={article} />
     </motion.div>

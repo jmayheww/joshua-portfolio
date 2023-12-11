40a3b4ca2846c92dcd80b3146d3a2e9ce4573930
Joshua <joshsmayhew@gmail.com>
2023-12-11 23:25:23 +0700

    implement responsive checks on project description and tech content to toggle show more or less for large device side by side views

--- app/components/ProjectCard.js
+++ app/components/ProjectCard.js
@@ -1,86 +1,106 @@
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export const ProjectCard = ({ project }) => {
   const [imageLoaded, setImageLoaded] = useState(false);
-  console.log("imageLoaded: ", imageLoaded);
-  const onImageLoad = () => {
-    setImageLoaded(true);
-  };
+  const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
+  const [isTechExpanded, setIsTechExpanded] = useState(false);
 
-  useEffect(() => {
-    onImageLoad();
-  }, [imageLoaded]);
+  const isLargeScreen = () => window.innerWidth >= 1024; // Responsive check (consider using a more React-friendly approach)
 
   return (
-    <div className="card bg-neutral rounded-sm shadow-md transition-transform duration-200 p-4 md:p-6 mx-auto lg:max-w-3xl">
-      {/* --- Image Section --- */}
+    <div className="card bg-neutral rounded-lg shadow-md transition-transform duration-200 p-4 md:p-6 mx-auto lg:max-w-3xl">
+      {/* Image Section */}
       <figure
         className={`relative w-full ${
           project.isEmailProject
-            ? "aspect-w-1 aspect-h-1 mb-[-1em]" // Adjust the negative margin as needed
+            ? "aspect-w-1 aspect-h-1 mb-[-1em]"
             : "aspect-w-16 aspect-h-9 mb-4"
-        } md:aspect-w-4 md:aspect-h-3 lg:aspect-w-16 lg:aspect-h-9 xl:aspect-w-4 xl:aspect-h-3 rounded-sm overflow-hidden`}
+        } md:aspect-w-4 md:aspect-h-3 lg:aspect-w-16 lg:aspect-h-9 xl:aspect-w-4 xl:aspect-h-3 rounded-lg overflow-hidden`}
       >
         {!imageLoaded && (
           <div
             className="animate-pulse bg-gray-200"
-            style={{ height: project.isEmailProject ? "300px" : "300px" }} // Adjust the height as needed
+            style={{ height: project.isEmailProject ? "300px" : "300px" }}
           ></div>
         )}
-
-        {project.isEmailProject ? (
-          <a href={project.links.demo} target="_blank" rel="noreferrer">
-            <img
-              src={project.canvaMockup}
-              alt={project.title}
-              className={`object-cover w-full h-full rounded-sm ${
-                imageLoaded ? "block" : "hidden"
-              }`}
-              onLoad={onImageLoad}
-              loading="lazy"
-              style={{ display: "block", lineHeight: 0 }} // Remove space caused by line-height
-            />
-          </a>
-        ) : (
-          <img
-            src={project.canvaMockup || project.desktopMockup}
-            alt={project.title}
-            className={`object-cover w-full h-full rounded-sm ${
-              imageLoaded ? "block" : "hidden"
-            }`}
-            onLoad={onImageLoad}
-            loading="lazy"
-            style={{ display: "block", lineHeight: 0 }} // Remove space caused by line-height
-          />
-        )}
+        <img
+          src={project.canvaMockup || project.desktopMockup}
+          alt={project.title}
+          className={`object-cover w-full h-full rounded-lg transition-opacity duration-500 ${
+            imageLoaded ? "opacity-100" : "opacity-0"
+          }`}
+          onLoad={() => setImageLoaded(true)}
+          loading="lazy"
+        />
       </figure>
 
-      {/* --- Content Section --- */}
-      <article className="flex flex-col justify-between space-y-4">
-        {/* Content Details */}
+      {/* Content Section */}
+      <article className="flex flex-col space-y-4">
+        {/* Title */}
+        <h2 className="text-xl font-semibold mb-2 bg-gradient-to-r from-primary to-neutral font-mono p-2 rounded-lg">
+          {project.title}
+        </h2>
+
+        {/* Description and See More Button */}
         <div>
-          <h2 className="text-xl font-semibold mb-2 bg-gradient-to-r from-primary to-neutral font-mono font-bold p-2 rounded-sm">
-            {project.title}
-          </h2>
-          <p className="text-gray-300 mb-4 leading-relaxed">
+          <p
+            className={`text-gray-300 ${
+              isDescriptionExpanded ? "" : "line-clamp-3"
+            }`}
+          >
             {project.description}
-          </p>{" "}
-          {/* Technologies */}
-          <div className="flex flex-wrap gap-2 mb-2">
-            {project.technologies.map((tech) => (
+          </p>
+          {(project.description.length > 100 ||
+            project.description.split(" ").length > 20) && (
+            <button
+              onClick={() => setIsDescriptionExpanded(!isDescriptionExpanded)}
+              className="text-primary hover:text-primary-dark text-sm mt-2"
+            >
+              {isDescriptionExpanded ? "See Less" : "See More"}
+            </button>
+          )}
+        </div>
+
+        {/* Technologies and More Button */}
+        <div className="flex flex-wrap gap-2 mb-4">
+          {project.technologies
+            .slice(
+              0,
+              isTechExpanded || !isLargeScreen()
+                ? project.technologies.length
+                : 3
+            )
+            .map((tech, index) => (
               <span
-                key={tech}
-                className="text-xs font-mono bg-gray-800 hover:bg-primary text-white py-0.5 px-2 rounded-sm transition-transform duration-200 "
+                key={index}
+                className="text-xs font-mono bg-gray-800 hover:bg-primary text-white py-0.5 px-2 rounded-sm transition-transform duration-200"
               >
                 {tech}
               </span>
             ))}
-          </div>
+          {!isTechExpanded &&
+            isLargeScreen() &&
+            project.technologies.length > 3 && (
+              <button
+                onClick={() => setIsTechExpanded(true)}
+                className="text-primary hover:text-primary-dark text-sm lg:inline-block hidden"
+              >
+                +{project.technologies.length - 3} More
+              </button>
+            )}
+          {isTechExpanded && isLargeScreen() && (
+            <button
+              onClick={() => setIsTechExpanded(false)}
+              className="text-primary hover:text-primary-dark text-sm lg:inline-block hidden"
+            >
+              See Less
+            </button>
+          )}
         </div>
+
         {/* Project Links */}
-        <div className="flex justify-end space-x-4">
-          {/* Added 'justify-end' to move the buttons to the right */}
+        <div className="flex justify-end space-x-4 mt-4">
           {project.links.github && (
             <a
               href={project.links.github}
